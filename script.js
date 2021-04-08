e6f4631b92b0625acb989647f21992f20ca6b5dd
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-04-08 17:37:34 +0200

    sortato per anno

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const App = new Vue({
 
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then(function(response){
+                response.data.response.sort(function (a, b) {
+                    var keyA = a.year;
+                        keyB = b.year;
+                    if (keyA < keyB) return -1;
+                    if (keyA > keyB) return 1;
+                    return 0;
+                });
                 Self.albums = response.data.response;
                 response.data.response.forEach(element => {
                     if (!Self.genres.includes(element.genre)) {
