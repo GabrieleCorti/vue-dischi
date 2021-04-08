4134999fa6efaa16fee402f8b30a2abc0ecc7d07
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-04-08 17:23:11 +0200

    fatto la select, ma non funziona per all

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const App = new Vue({
     el: "#app",
     data: {
-        albums: []
+        albums: [],
+        genres: [],
+        selectedGenre: "All"
     },
     mounted: function(){
+        const Self = this;
+
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then(function(response){
-                console.log(response);
+                Self.albums = response.data.response;
+                response.data.response.forEach(element => {
+                    if (!Self.genres.includes(element.genre)) {
+                        Self.genres.push(element.genre);
+                    }
+                });
             });
-    }
+    },
+
 })
