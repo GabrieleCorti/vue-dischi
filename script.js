ae4cd02cfa84e84d48004fda491e0f8ee9c50ee0
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-04-08 15:20:38 +0200

    inizio es

---
+++ script.js
@@ -0,0 +1,12 @@
+const App = new Vue({
+    el: "#app",
+    data: {
+        albums: []
+    },
+    mounted: function(){
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then(function(response){
+                console.log(response);
+            });
+    }
+})
